d90f11dc8e48c89f32549556e1ab47aa8ffb6597
Nghia <thanhnghiadk@gmail.com>
2021-08-05 10:38:43 +0900

    This is the configuration file for dividing the symbol and component classes.

---
+++ config.py
@@ -0,0 +1,12 @@
+# symbol and component class for semantic data generation
+CONFIG = {
+    "GROUP1": ['P', 'X', 'T', 'G', 'A', 'R', 'N', 'E', 'B', 'S', 'F', 'M', 'L', 'H', 'C', 'I', 'V', 'Y'],
+    "GROUP2": ['>', '<', '\\leq', '\\geq', '\\neq'],
+    "GROUP3": ['a', 'n', 'e', 'v', 'c', 'k', 'x', 'd', 'z', 'u', 's', 'f', 'r', 't', 'b', 'o', 'l', 'h', 'i'],
+    "GROUP3.2": ['p', 'g', 'q', 'y', 'j'],
+    "GROUP3.1": ['m', 'w'],
+    "GROUP4": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    "GROUP5": ['\\mu', '\\alpha', '\\gamma', '\\sigma', '\\beta', '\\theta', '\\lambda', "\\phi"],
+    "SAME": ["\\infty", '\\pi', '\\times', '\\div', '\\pm', '=', '+', '\\cos', '\\sin', '\\tan', '\\rightarrow', '\\in'],
+    "NOT_CHANGE": ['\\int', '\\sqrt', '|', '-', '\\prime', '\\log', '\\lim', '\\sum', '.', ',', '\\ldots', '\\circ', '\\triangle', '!', '(', ')', '\\{', '\\}', '[', ']']
+}
